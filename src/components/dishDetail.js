850ef07523442256234218779cbe2677f3a12a1e
AMEE LEPCHA <ameelepcha@gmail.com>
2021-04-23 12:51:45 +0530

    header and footer

--- src/components/dishDetail.js
+++ src/components/dishDetail.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 // class DishDetail extends Component {
